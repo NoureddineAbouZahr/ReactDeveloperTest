31b5aecd39b196c326ed691ba9651882b974d169
NoureddineAbouZahr <naz.199860@gmail.com>
2022-10-05 12:28:45 +0300

    setting the currency list for functionality

--- ecommerce/src/components/Currency.js
+++ ecommerce/src/components/Currency.js
@@ -3,21 +3,27 @@ import React, { Component } from 'react';
 class Currency extends Component {
     render() {
         return (
-            <div>
+            <div >
+                <div className='currencyContainer'>
+                    
                 <h3>$</h3>
-                <input type="checkbox" className='dropdown' />
                 <div class="arrow-icon"></div>
+                <input type="checkbox" className='dropdown' />
+                </div>
                 {/* <select className='currency'>
                     <option value="dollar">$ USD</option>
                     <option value="euro">€ EUR</option>
                     <option value="yen">¥ JPY </option>
                 </select> */}
-                <ol className='currency'>
+                <div className='currency' >
+
+                <ol >
                     <li>$ USD</li>
                     <li>€ EUR</li>
                     <li>¥ JPY</li>
                     
                 </ol>
+                </div>
             </div>
         );
     }
